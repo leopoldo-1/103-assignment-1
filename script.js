58f1519917e8e8d931a99b682889816f12d5c2b8
leo <61714687+leopoldo-1@users.noreply.github.com>
2022-03-01 18:12:53 -0800

    first commit
    adding index.html and script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+const NAME = "Leopoldo"
+const LAST_NAME = "Miranda"
+let school = "SDGKU"
+let hobbie = "movies theater"
+let favoriteFood = "Pizza"
+let favoriteColor = "red"
+let marvelHero1 = "Iron man"
+let marvelHero2 = "Hulk"
+let marvelhero3 = "Thor"
+let marvelHero4 = "Spiderman"
+let sport = "Basketball"
+
+let salary = 1000
+let dayOfTheYear = 365
+let daysOfTheWeek = 5
+let daysOfTheMoth = 12
+
+let professor = "Samantha Jimenez"
+const html = "Hyper Text Markup Language"
+const css = "Cascading Style Sheet"
+const js = "Javascript"
+let programmingLanguage = "React"
+
+let template = `Hello my name is <b>${NAME} ${LAST_NAME}</b>  i am studying in <b>${school}</b>,
+              my hobbie is go to ${hobbie}, favorite sport: ${sport}.
+
+              <hr/>
+              professor: ${professor},
+              <br/>
+              101,102 unit: ${html} and ${css},
+              <br/>
+              103 unit: ${js} and ${programmingLanguage}
+
+              <br><hr/>
+              <i>
+                the year has ${daysOfTheWeek} days
+                the year has ${daysOfTheMoth} months
+                the year has ${dayOfTheYear} days.
+              </i>
+`
+
+let template = `my name is ${name} i am ${age} ...... `
+
+document.write(template)
+
